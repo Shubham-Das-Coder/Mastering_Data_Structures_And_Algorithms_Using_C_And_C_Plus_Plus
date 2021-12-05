651de5d4562f04decd728ee6a8f69725328b0573
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-05 17:34:03 +0530

    Add files via upload

---
+++ Array_Basics_10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int A[10] = {2, 4, 6, 8, 10, 12};
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << A[i] << endl;
+    }
+
+    return 0;
+}
