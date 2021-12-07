3f5d4aaf02661ecd839fff7d7fef7be133f2a7fc
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-07 17:32:44 +0530

    Add files via upload

---
+++ Structures_4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Rectangle // Defining structure
+{
+    int length;
+    int breadth;
+} r1, r2, r3; // Declaring multiple variable along with definition and are global variables
+
+int main()
+{
+    return 0;
+}
