132621fcf139eab121008cb6fc4c26000a5bb0a9
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-07 17:35:01 +0530

    Add files via upload

---
+++ Structures_5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Rectangle // Defining structure
+{
+    int length;
+    int breadth;
+};
+
+struct Rectangle r1, r2, r3; // Declaring variables
+
+int main()
+{
+    return 0;
+}
