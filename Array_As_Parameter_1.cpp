4a66dcfeeab9126279f7809346e46e86a343254f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-11 08:26:16 +0530

    Add files via upload

---
+++ Array_As_Parameter_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A[] = {2, 4, 6, 8, 10};
+    int n = 5;
+
+    for (int x : A) // Using forish loop
+        cout << x;
+
+    return 0;
+}
