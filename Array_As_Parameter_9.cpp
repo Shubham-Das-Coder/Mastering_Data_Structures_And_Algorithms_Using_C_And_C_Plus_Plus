94a524b663db36967bda653cf0624700813ebf81
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-11 19:19:52 +0530

    Add files via upload

---
+++ Array_As_Parameter_9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void fun(int *A, int n)
+{
+    // When this array is passed by address then if we change anything inside this array will it change the actual array?
+    // Yes, because its a pointer whether you write * or [] it is a pointer only
+    // So when it is a pointer on changing the value it gets changed
+    A[0] = 15;
+}
+
+int main()
+{
+    int A[] = {2, 4, 6, 8, 10};
+    int n = 5;
+    fun(A, n);
+    for (int x : A)
+        cout << x << "  ";
+
+    return 0;
+}
