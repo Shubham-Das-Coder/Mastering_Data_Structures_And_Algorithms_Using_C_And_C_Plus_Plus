827f9643237c280cf52644970a80fdef9b588525
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-18 11:10:52 +0530

    Add files via upload

---
+++ Static_And_Global_In_Recursion_6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int x = 0; // Creating a global variable and assigning it as zero
+// It is assigned outside all thwee variables so it can be accessed by all the functions
+
+int fun(int n) // Creating a function named fun with return type int and taking a parameter int n
+{
+    if (n > 0)
+    {
+        x++;
+        return fun(n - 1) + x;
+    }
+    return 0;
+}
+
+int main()
+{
+    int r;
+    printf("Enter the number\n");
+    scanf("%d", &r);
+    r = fun(r);
+    printf("%d\n", r);
+    return 0;
+}
