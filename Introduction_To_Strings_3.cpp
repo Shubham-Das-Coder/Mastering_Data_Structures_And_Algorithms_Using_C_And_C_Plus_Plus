eed19c600cde3d5a2e2267eb7d3ba695474ae3eb
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-25 21:36:53 +0530

    Add files via upload

---
+++ Introduction_To_Strings_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;            // Creating a character variable
+    ch = 'A';           // Initializing the character variable
+    cout << ch << endl; // Printing the character
+
+    return 0;
+}
