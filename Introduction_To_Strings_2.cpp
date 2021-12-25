f4acc220699e8a1e51e5e47bad2a399d2612354d
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-25 20:50:33 +0530

    Add files via upload

---
+++ Introduction_To_Strings_2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;          // Creating a character variable
+    ch = 'A';         // Initializing the character variable
+    printf("%d", ch); // Printing the ASCII value of the character
+
+    return 0;
+}
