88c7853b94f69a39e9a8693cd338bb52efa77e05
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-25 20:49:34 +0530

    Add files via upload

---
+++ Introduction_To_Strings_1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;          // Creating a character variable
+    ch = 'A';         // Initializing the character variable
+    printf("%c", ch); // Printing the character
+
+    return 0;
+}
