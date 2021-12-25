b6797731947856f3956b2d0417344babd711abf7
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-25 22:05:44 +0530

    Add files via upload

---
+++ Finding_Length_Of_A_String_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char s[] = "Welcome";
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+    }
+
+    cout << "Length is " << i;
+
+    return 0;
+}
