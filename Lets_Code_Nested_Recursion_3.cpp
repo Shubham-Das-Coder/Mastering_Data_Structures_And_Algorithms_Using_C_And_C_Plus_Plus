6bbb6913232550e0f6f74ca7715e91d2c079a29d
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-11 20:02:24 +0530

    Add files via upload

---
+++ Lets_Code_Nested_Recursion_3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Writing a function fun which takes parameter n of integer type
+
+int fun(int n)
+{
+    if (n > 100) // If n s greater than 100
+        return n - 10;
+    return fun(fun(n + 11)); // Else will return this value
+}
+
+int main()
+{
+    int r;        // Taking some temporary variable named r
+    r = fun(100); // Calling the function fun and passing value 100 and storing the result in variable r
+    printf("%d\n", r);
+    return 0;
+}
